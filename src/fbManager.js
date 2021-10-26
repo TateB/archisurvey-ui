e9f00604dcd27c45c3bbfa6c2c63ee032baa208f
TateB <me@tatebulic.com.au>
2021-10-26 23:34:24 +1100

    fixed firebase leak lol

--- src/fbManager.js
+++ src/fbManager.js
@@ -1,14 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { collection, getDocs, getFirestore } from "firebase/firestore";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyCONJxh_kbqX6QqulrgKb5grmr6xOls6p4",
-  authDomain: "archisurvey.firebaseapp.com",
-  projectId: "archisurvey",
-  storageBucket: "archisurvey.appspot.com",
-  messagingSenderId: "228944202121",
-  appId: "1:228944202121:web:03949f1941c266b3907958",
-};
+import { firebaseConfig } from "./fbconfig";
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore();
